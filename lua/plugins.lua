107ec274f6f76f25a9755f2a143fb22373f266d5
Kevin Schweikert <kevin@kevinschweikert.de>
2021-09-19 21:27:17 +0200

    Initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,37 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'kabouzeid/nvim-lspinstall'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
+	use 'joshdick/onedark.vim'
+	use {
+	  'hoob3rt/lualine.nvim',
+		  requires = {'kyazdani42/nvim-web-devicons', opt = true}
+	}
+	use 'ryanoasis/vim-devicons'
+	use {
+	  'nvim-telescope/telescope.nvim',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+	use {
+	    'kyazdani42/nvim-tree.lua',
+	    requires = 'kyazdani42/nvim-web-devicons'
+	}
+use "projekt0n/github-nvim-theme"
+end)
